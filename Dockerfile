c31a7fe607d525b207b1d17ed0c0a67219305048
tamnguyen-cse <tamnguyen.cse@gmail.com>
2021-08-27 19:19:21 +0700

    Switch to use Inet Address

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 COPY build/libs/*.jar app.jar
-COPY entrypoint.sh /usr/local/bin/
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/ace/demo/configuration/Application.java
+++ src/main/java/com/ace/demo/configuration/Application.java
@@ -1,36 +1,45 @@
 package com.ace.demo.configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
 
-    // private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    // @Value("${server.port}")
-    // private int port;
-    //
-    // @Bean
-    // @Primary
-    // public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-    // EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
-    // String ip = null;
-    // try {
-    // ip = InetAddress.getLocalHost().getHostAddress();
-    // } catch (UnknownHostException e) {
-    // logger.error("Exception: {}", e);
-    // }
-    // config.setIpAddress(ip);
-    // config.setNonSecurePort(port);
-    // config.setPreferIpAddress(true);
-    // return config;
-    // }
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    @Primary
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            logger.error("Exception: {}", e);
+        }
+        config.setIpAddress(ip);
+        config.setNonSecurePort(port);
+        config.setPreferIpAddress(true);
+        return config;
+    }
 
 }
